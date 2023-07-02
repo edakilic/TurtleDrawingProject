f075332ba6dd2b5db875cbb50eaa12d8db6a75b3
Eda Kılıç <klc-eda@outlook.com>
2023-07-02 15:41:49 +0300

    turtle drawing examples added

---
+++ polygon.py
@@ -0,0 +1,10 @@
+turtle_instance = turte.Turtle()
+num_sides = ###
+angle = 360.0 / num_sides
+side_length = ###
+
+for i in range(num_sides):
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_length)
+
+turtle.done()
